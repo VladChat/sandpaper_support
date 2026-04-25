398eda683359be26525e558abd70a83fd6aaf7ce
VladChat <vladislav.doroshenko@gmail.com>
2026-04-24 19:50:07 -0500

    Configure Supabase frontend connection

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 window.eQualleConfig = window.eQualleConfig || {};
 
 window.eQualleConfig.SUPABASE_URL =
-  window.eQualleConfig.SUPABASE_URL || window.SUPABASE_URL || "";
+  window.eQualleConfig.SUPABASE_URL || "https://cmaaykcxcfxrujkrqsbt.supabase.co";
 
 window.eQualleConfig.SUPABASE_ANON_KEY =
-  window.eQualleConfig.SUPABASE_ANON_KEY || window.SUPABASE_ANON_KEY || "";
+  window.eQualleConfig.SUPABASE_ANON_KEY || "sb_publishable_4ePKD0oekKoLM80ZCOwg7w_OXLWHHIE";
